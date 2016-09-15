496fa677633b3fec9ada18513b57280b1cdb41c9
Dylan7575 <dcl75@nau.edu>
2016-09-15 01:46:38 -0700

    more done
    more done

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
+
 void p6_to_p3(FILE*);
 void p3_to_p6(FILE*);
+void parse_white_space(FILE*);
 
 int main() {
     FILE * fp ;
-    fp= fopen("C:\\Users\\Dylan\\ClionProjects\\project1CS430\\boxes_1.ppm","r");
+    fp= fopen("C:\\Users\\Dylan\\ClionProjects\\project1CS430\\smile.ppm","r");
     //opening file
     if (fp==NULL){
         perror("That file does not exist");
@@ -18,16 +21,19 @@ int main() {
         char header[header_size];
         //creating buffer for checking what format the file is in
         result=fread(header,1,header_size,fp);
+
         //reading in file format
         if(result!=header_size){
             perror("Could not read your file");
             //error checking file reading
         }
         if(header[0]=='P'&&header[1]=='6'){
+           // printf("req");
             p6_to_p3(fp);
             //if p6 sending it to function for converting to p3
         }
         else if (header[0]=='P'&&header[1]=='3'){
+            //printf("req");
             p3_to_p6(fp);
             //if p3 sending it to function for converting to p6
         }
@@ -45,21 +51,112 @@ int main() {
 
     return 0;
 }
+
 void p6_to_p3(FILE * fp){
-    int width,height,whitespace=3;
-    int buf;
-    fgetc(fp);
-    whitespace--;
-    printf("%c,%i",buf,whitespace);
+    int *width,*height;
+
+    if(fgetc(fp)=='#'){
+        printf("req");
+        while(fgetc(fp)!=' '){
+            fseek(fp,1,SEEK_CUR);
+        }
+    }
+    parse_white_space(fp);
 
 
 
-    printf("%c",buf);
+    //int temp = fgetc(fp);
+    //printf("%c",temp);
+
+
+
 
 
 
     //nothing yet
 }
+
 void p3_to_p6(FILE * fp){
-    int width,height;
+    int width,height,temp,max,temp2,i=0;
+    parse_white_space(fp);
+    int* temp3;
+    do temp=fgetc(fp);while(temp!='\n');
+    //int* temp2=get_num(fp);
+    fscanf(fp,"%i ",&height);
+    fscanf(fp,"%i ",&width);
+    fscanf(fp,"%i ",&max);
+    printf("%i",height*width);
+    int buffer[width*height*10];
+
+
+    int f =ftell(fp);
+    fseek(fp,-f,SEEK_CUR);
+    int header[f];
+    fread(header, 1, f,fp);
+    //printf("%c",*header);
+    FILE* output=fopen("C:\\Users\\Dylan\\ClionProjects\\project1CS430\\output.ppm","w+");
+    fwrite(header,1, f,output);
+    fclose(output);
+    //fgets(buffer,(height*width)-f,fp);
+    //fread(buffer,1,width*height,fp);
+    //atoi(buffer);
+   // fwrite(buffer,1,width*height,output);
+
+
+/*
+    do{
+        temp2=fgetc(fp);
+        buffer[i++]=temp2;
+
+    } while(temp2!=' ');
+    fwrite(buffer,1,width*height,output);
+    */
+
+
+
+
+
+
+
+
+
+
+
+
+    //
+
+
+    //fseek(fp,0,SEEK_END);
+ //   int a = ftell(fp);
+  //  printf("%i",a);
+   // fsetpos(fp,f);
+   // fread(buffer,1,a-f,fp);
+   // fwrite(buffer,1,a-f,output);
+
+
+
+
+    //fseek(output,1,SEEK_SET);
+    //fwrite("6",1,1,output);
+
+
+    // *buffer=fgetc(fp);
+    //temp2 =fgetc(fp);
+ //   printf("%c %i",header[2], f);
+
+
+
+
+
+
+
+
+
+
 }
+void parse_white_space(FILE *fp){
+    while (fgetc(fp)==' '){
+        fseek(fp,1,SEEK_CUR);
+    }
+}
+
